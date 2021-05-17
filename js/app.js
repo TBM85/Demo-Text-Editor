c6da3f010aef818768256c177ff7d2eedeeb30d9
TBM85 <tballesterm@gmail.com>
2021-05-16 20:10:39 -0400

    Create the structure of the text editor

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const header = document.querySelector(".header");
+
+const h1 = document.createElement("h1");
+header.appendChild(h1);
+h1.innerHTML = "Text Editor";
+
+const toolbar = document.querySelector(".toolbar");
+
+const textBox = document.querySelector(".text-box");
