cf2613127a7020b52663bcf7fe0f11141a813020
TBM85 <tballesterm@gmail.com>
2021-05-18 16:05:37 -0400

    Create a selection list for font size

--- js/app.js
+++ js/app.js
@@ -1,43 +1,67 @@
 const btns = [
+  { class: "toolbar__btn-text-style-bold", dataset: "bold" },
+  { class: "toolbar__btn-text-style-italic", dataset: "italic" },
+  { class: "toolbar__btn-text-style-underline", dataset: "underline" },
   { class: "toolbar__btn-text-align-left", dataset: "justifyLeft" },
   { class: "toolbar__btn-text-align-center", dataset: "justifyCenter" },
   { class: "toolbar__btn-text-align-right", dataset: "justifyRight" },
   { class: "toolbar__btn-text-align-justify", dataset: "justifyFull" },
-  { class: "toolbar__btn-text-style-bold", dataset: "bold" },
-  { class: "toolbar__btn-text-style-italic", dataset: "italic" },
-  { class: "toolbar__btn-text-style-underline", dataset: "underline" },
   { class: "toolbar__btn-text-list-ordered", dataset: "insertOrderedList" },
-  { class: "toolbar__btn-text-list-unordered", dataset: "insertUnorderedList" }
-]
+  { class: "toolbar__btn-text-list-unordered", dataset: "insertUnorderedList" },
+];
 
-// HEADER 
+// HEADER
 const header = document.querySelector(".header");
 
 const h1 = document.createElement("h1");
 header.appendChild(h1);
 h1.innerHTML = "Text Editor";
 
-
 // TOOLBAR
 const toolbar = document.querySelector(".toolbar");
 
-const buttons = document.createElement("div");
-toolbar.appendChild(buttons);
-buttons.classList.add("toolbar__buttons");
+const toolbarContainer = document.createElement("div");
+toolbar.appendChild(toolbarContainer);
+toolbarContainer.classList.add("toolbar__container");
 
-// TOOLBAR BUTTONS (Text Align)
-const buttonsTextAlign = document.createElement("div");
-buttons.appendChild(buttonsTextAlign);
-buttonsTextAlign.classList.add("toolbar__btn-text-align");
+// TOOLBAR SELECT (Font Size)
+const fontSize = document.createElement("div");
+toolbarContainer.appendChild(fontSize);
+fontSize.classList.add("toolbar__select-font-size");
+
+const labelFontSize = document.createElement("label");
+fontSize.appendChild(labelFontSize);
+labelFontSize.htmlFor = "font-size";
+labelFontSize.innerHTML = "Font Size";
+
+const selectFontSize = document.createElement("select");
+fontSize.appendChild(selectFontSize);
+selectFontSize.id = "font-size";
+
+for (let i = 3; i <= 7; i++) {
+  const option = document.createElement("option");
+  selectFontSize.appendChild(option);
+  option.value = i;
+  option.innerHTML = i * 4 + " px";
+
+  selectFontSize.addEventListener("change", (event) => {
+    document.execCommand("fontSize", false, event.target.value);
+  });
+}
 
 // TOOLBAR BUTTONS (Text Style)
 const buttonsTextStyle = document.createElement("div");
-buttons.appendChild(buttonsTextStyle);
+toolbarContainer.appendChild(buttonsTextStyle);
 buttonsTextStyle.classList.add("toolbar__btn-text-style");
+
+// TOOLBAR BUTTONS (Text Align)
+const buttonsTextAlign = document.createElement("div");
+toolbarContainer.appendChild(buttonsTextAlign);
+buttonsTextAlign.classList.add("toolbar__btn-text-align");
 
 // TOOLBAR BUTTONS (Text List)
 const buttonsTextList = document.createElement("div");
-buttons.appendChild(buttonsTextList);
+toolbarContainer.appendChild(buttonsTextList);
 buttonsTextList.classList.add("toolbar__btn-text-list");
 
 let containerBtnName;
@@ -57,9 +81,9 @@ for (let btn of btns) {
   generalBtn.dataset["command"] = `${btn.dataset}`;
 
   // Executes the specified command for the selected part
-  generalBtn.addEventListener('click', () => {
-		document.execCommand(`${btn.dataset}`, false, null);
-	});
+  generalBtn.addEventListener("click", () => {
+    document.execCommand(`${btn.dataset}`, false, null);
+  });
 }
 
 // TEXT-BOX
